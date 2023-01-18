7ae055325fe376a581d3f85d85c08d6f114d849e
= <=>
2023-01-18 17:38:10 -0300

    incluindo permissão para api de usuarios e testando basic auth UserDetailServiceImpl

--- src/main/java/io/github/dougllasfps/config/SecurityConfig.java
+++ src/main/java/io/github/dougllasfps/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.github.dougllasfps.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,6 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/produtos/**").hasRole("ADMIN")
                 .antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic(); // autenticação via header
         // .formLogin(); // cria o formulario de login do spring security ou é possivel
